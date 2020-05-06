3fcb53e78a8440f3248dd746d8016d658dd0f875
ssulivan11 <ssulivan11@gmail.com>
2020-05-06 10:15:53 -0500

    initial

---
+++ main.jsx
@@ -0,0 +1,4 @@
+import { h, render } from "preact";
+import { App } from "./src/app.jsx";
+
+render(<App name={"tiny project"} />, document.getElementById("app"));
---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import { h } from "preact";
+
+export const App = ({ name }) => (
+  <div className="container">
+    <div className="banner">
+      <h1>Welcome to {name}</h1>
+      <p>
+        This is just a simple demo project messing with vite for a dev server
+        and rollup as a tiny bundler.
+      </p>
+    </div>
+  </div>
+);
