54c0f000246f9d6ca55aeb5b229791de6fdd5b6e
ssulivan11 <ssulivan11@gmail.com>
2020-05-09 18:12:20 -0500

    perfect setup

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# tiny-project
+# tiny-project 🐁
 
-This is just a simple demo project messing with [vite](https://github.com/vuejs/vite#bare-module-resolving) for the dev server, insanely fast server, no bundling and hmr.
+This is just a simple demo project messing with [vite](https://github.com/vuejs/vite#bare-module-resolving) for the dev server, stupid fast, no bundling and hmr.
+
 Also utilizing [parcel](https://github.com/parcel-bundler/parcel) as the bundler.
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,13 +1,20 @@
-import { h } from "preact";
+import { h, FunctionalComponent } from "preact";
 
-export const App = ({ name }) => (
-  <div className="container">
-    <div className="banner">
-      <h1>Welcome to {name}!</h1>
-      <p>
-        This is just a simple demo project messing with vite for a dev server
-        and rollup as a tiny bundler.
-      </p>
+export interface Props {
+  name: string;
+}
+
+export const App: FunctionalComponent<Props> = ({ name }: Props) => {
+  return (
+    <div className="container">
+      <div className="banner">
+        <h1>Welcome to {name}!</h1>
+        <p>
+          This is just a simple demo project messing with vite for the dev
+          server, stupid fast, no bundling and hmr.
+        </p>
+        <p>Also utilizing parcel as the bundler.</p>
+      </div>
     </div>
-  </div>
-);
+  );
+};
